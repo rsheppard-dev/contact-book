8b3e1fcaee89a314d9e17b9bbb79b611a74b2c67
Roy Sheppard <rsheppard83@gmail.com>
2023-02-20 22:46:30 +0000

    added user endpoints and bcrypt to hash passwords

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,8 @@
 import Image from 'next/image';
 import { Poppins } from '@next/font/google';
 
-async function getUsers() {
-	const res = await fetch(`${process.env.BASE_URL}/api/user`);
-
-	if (!res.ok) {
-		console.log(res);
-	}
-
-	return res.json();
-}
-
 const inter = Poppins({ subsets: ['latin'], weight: ['400'] });
 
 export default async function Home() {
-	const data = await getUsers();
 	return <main></main>;
 }
--- pages/api/user/index.ts
+++ pages/api/user/index.ts
@@ -1,7 +1,7 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 import { prisma } from '../../../prisma/client';
+import { hash } from 'bcrypt';
 
 type userProps = {
 	firstName: string;
@@ -14,36 +14,41 @@ export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse
 ) {
-	const user: userProps = JSON.parse(req.body);
-
 	// get: /api/user
 	if (req.method === 'GET') {
 		try {
 			const data = await prisma.user.findMany();
 
-			return res.status(200).send(data);
+			res.status(200).send(data);
 		} catch (error) {
-			return res.status(500).send(error);
+			res.status(500).send(error);
 		}
 	}
 
 	// post: /api/user
 	if (req.method === 'POST') {
-		try {
-			const data = await prisma.user.create({
-				data: {
-					firstName: user.firstName,
-					lastName: user.lastName,
-					email: user.email,
-					password: user.password,
-				},
-			});
+		// get data from user
+		const user: userProps = req.body;
 
-			return res.status(200).send(data);
-		} catch (error) {
-			return res
-				.status(500)
-				.send({ message: 'Error: Failed to create new user.', error });
-		}
+		// hash password
+		hash(user.password, 10, async function (err, hash) {
+			try {
+				// create new user and add to postgres database
+				const data = await prisma.user.create({
+					data: {
+						firstName: user.firstName,
+						lastName: user.lastName,
+						email: user.email,
+						password: hash,
+					},
+				});
+
+				res.status(200).send(data);
+			} catch (error) {
+				res
+					.status(500)
+					.send({ message: 'Error: Failed to create new user.', error });
+			}
+		});
 	}
 }
